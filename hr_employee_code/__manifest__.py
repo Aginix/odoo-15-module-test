f59bf96da58e6a731e249e3a911502184782fd0e
nopparuts <ak3.nopparut@gmail.com>
2025-08-04 10:18:56 +0700

    wip runbot

--- hr_employee_code/__manifest__.py
+++ hr_employee_code/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "HR Employee Code",
-    "version": "16.0.1.1.12",
+    "version": "16.0.1.1.13",
     "description": "HR Employee Code",
     "summary": "HR Employee Code",
     "website": "https://github.com/aginix/kmitl-odoo",
